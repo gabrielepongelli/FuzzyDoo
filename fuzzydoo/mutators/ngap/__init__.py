adc6f6cdaf2cf63df380e8087a7d69859a34457a
Gabriele Pongelli <pongelligabriele@gmail.com>
2024-10-14 10:57:06 +0200

    Added mutator for RelOid type.

--- fuzzydoo/mutators/ngap/__init__.py
+++ fuzzydoo/mutators/ngap/__init__.py
@@ -3,3 +3,4 @@ from .int import *
 from .real import *
 from .enum import *
 from .oid import *
+from .rel_oid import *
---
+++ fuzzydoo/mutators/ngap/rel_oid.py
@@ -0,0 +1,1 @@
+# since there is no Information Element defined which contains a REL_OID value, I skipped this.
