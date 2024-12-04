aea3b4b2336094a367465eb80e57af2d0987065d
Gabriele Pongelli <pongelligabriele@gmail.com>
2024-12-04 12:17:08 +0100

    Reverted some changes from the previous commit.

--- fuzzydoo/proto/ngap/messages.py
+++ fuzzydoo/proto/ngap/messages.py
@@ -137,6 +137,9 @@ class NGAPMessage(Message):
                 res = self._content.get_at(p)
                 return map_type(type(res))(content=res, path=redacted, parent=self)
 
+        if len(parts) == 2 and hasattr(self, str(parts[1])) and str(parts[1]) != 'content':
+            return getattr(self, parts[1])
+
         raise ContentNotFoundError(f"No content at the path '{qname}' exists in the message")
 
     def set_content(self, qname: str, value: Any):
@@ -152,6 +155,8 @@ class NGAPMessage(Message):
 
         full_path = self._restore_path(parts[1:])
         if full_path in self._leaf_paths:
+            # first try to search in the message content
+
             if isinstance(value, ASN1Type):
                 value = value.value
 
@@ -161,6 +166,9 @@ class NGAPMessage(Message):
                 self._modified_proc_code = value
             else:
                 self._content.set_val_at(full_path, value)
+        elif len(parts) == 2 and hasattr(self, str(parts[1])) and str(parts[1]) != 'content':
+            # then try with some other attributes from the parent class
+            setattr(self, parts[1], value)
         else:
             raise ContentNotFoundError(f"No content at the path '{qname}' exists in the message")
 
